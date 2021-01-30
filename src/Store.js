449c177059dbb9192eaba0233da2dfe0e433301a
Maxim Betin <betinmaxim@gmail.com>
2021-01-30 10:49:57 +0100

    Repos fix

--- src/Store.js
+++ src/Store.js
@@ -35,42 +35,18 @@ export default new Vuex.Store({
         for (var repo of res.data) {
           var repo_name = repo.name;
 
-          //If not a forked repo
-          if (repo_name.indexOf("maxim-") >= 0) {
-            const languages_res = await axios.get(repo.url + "/languages");
-            var updated_at = repo.updated_at.split("T")[0];
+          const languages_res = await axios.get(repo.url + "/languages");
+          var updated_at = repo.updated_at.split("T")[0];
 
-            data.push({
-              name: repo_name,
-              link: repo.html_url,
-              stars: repo.stargazers_count,
-              description: repo.description,
-              updated_at: updated_at,
-              languages: languages_res.data,
-              fork: repo.fork
-            });
-          }
-        }
-
-        //Push forked (I know, code redundancy)
-        for (var repo2 of res.data) {
-          var repo_name2 = repo.name;
-
-          //If a forked repo
-          if (repo_name.indexOf("maxim-") === -1) {
-            const languages_res = await axios.get(repo.url + "/languages");
-            var updated_at2 = repo.updated_at.split("T")[0];
-
-            data.push({
-              name: repo_name2,
-              link: repo2.html_url,
-              stars: repo2.stargazers_count,
-              description: repo2.description,
-              updated_at: updated_at2,
-              languages: languages_res.data,
-              fork: repo2.fork
-            });
-          }
+          data.push({
+            name: repo_name,
+            link: repo.html_url,
+            stars: repo.stargazers_count,
+            description: repo.description,
+            updated_at: updated_at,
+            languages: languages_res.data,
+            fork: repo.fork
+          });
         }
 
         commit("setRepositories", data);
